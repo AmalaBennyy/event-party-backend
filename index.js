18ff6d5fa4a8efcf64c8591d78c1ed07a116eabc
AryamolMS <aryamolsimi@gmail.com>
2023-12-01 21:25:34 +0530

    backend_final

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 //import json-server library in index.js
 const jsonServer=require('json-server')
 //create server using json-server
-const mediaPlayerServer=jsonServer.create()
+const eventserver=jsonServer.create()
 //create path to db.json file
 const router=jsonServer.router('db.json')
 //middlewares->js to json
 const middleware=jsonServer.defaults()
 //connect middleware and router in server
-mediaPlayerServer.use(middleware)
-mediaPlayerServer.use(router)
+eventserver.use(middleware)
+eventserver.use(router)
 //setup port to server
-const port=4000 || process.env.PORT
+const port=5000 || process.env.PORT
 //to listen server for resolving request 
-mediaPlayerServer.listen(port,()=>{
-    console.log(`mediaPlayerServer started at ${port} and ready fetch request`);
+eventserver.listen(port,()=>{
+    console.log(`eventserver started at ${port} and ready fetch request`);
 })
